360699436354cc1f217b601cf5365e83ebadb6ad
and1fa <andifairulamri@gmail.com>
2024-09-20 14:57:54 +0800

    Add files via upload

---
+++ Praktikum APD/Kelas/Pertemuan 3/Cuaca.py
@@ -0,0 +1,75 @@
+# cuaca = "cerah"
+
+# if cuaca == "cerah":
+#     print("kamu pergi keluar")
+
+# variabel akan bernilai false jika string kosong, 0, (), {}, [], "", False
+
+# uang = 20000
+
+# if uang:
+#     print("pergi ke pasar")
+# else:
+#     print("pergi ke rumah teman")
+
+
+# bilangan = 1
+
+# if bilangan > 0:
+#     print("Bilangan positif")
+
+# else:
+#     print("bilangan negatif")
+
+
+# Umur = int(input("Masukkan umur Anda : "))
+# if Umur <= 10:
+#     print("Anda anak-anak")
+# elif Umur <= 18:
+#     print("Anda remaja")
+# elif Umur <=35:
+#     print("Anda dewasa")
+# elif Umur <=65:
+#     print("Anda paru baya")
+# else:
+#     print("Anda Tua")
+
+
+# nilai = 76
+
+# if nilai >=80:
+#     if nilai >= 85:
+#         print("nilai A+")
+#     else :
+#         print("Nilai A-")
+# else :
+#     print ("tidak lulus")
+
+
+# print ("Menu : 1. Nonton film 2. Ngoding 3. Keluar")
+
+# pilihan = int(input("masukkan pilihan anda: "))
+
+# if pilihan == 1:
+#     print ("sedang nonton film")
+# elif pilihan == 2:
+#     print ("sedang ngoding")
+# elif pilihan == 3:
+#     print ("keluar dari aplikasi")
+# else : 
+#     print ("menu tidak tersedia")
+
+bilangan = 10
+# pengkondisian pertama 
+
+if bilangan % 2 = 0:
+    print ("bilangan genap")
+if bilangan > 0:
+    print ("bilangan positif")
+
+# pengkondisian kedua
+
+if bilangan % 2 = 0:
+    print ("bilangan genap")
+elif bilangan > 0:
+    print ("bilangan positif")

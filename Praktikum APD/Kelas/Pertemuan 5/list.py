b206a3f23f73f284245d07f75fae7e380a289512
and1fa <andifairulamri@gmail.com>
2024-10-04 15:57:11 +0800

    Add files via upload

---
+++ Praktikum APD/Kelas/Pertemuan 5/list.py
@@ -0,0 +1,72 @@
+# data = [1,2,3,4, True, [2,3],"adi"]
+# data2 = list([1,2,3,4,5])
+
+# print(data[6])
+# print(data[5][0])
+# print(type(data2))
+
+# data = ["apel", "durian", "jeruk"]
+# print(data)
+# data.append("semangka")
+# print(data)
+# data.insert(0,"rambutan")
+# print(data)
+# data.extend(["anggur", "melon"])
+# print(data)
+# print(len(data))
+# print(data)
+# data[0]= "anggur"
+# data[0:2]= "anggur","semangka"
+# print(data)
+
+# del data[2]
+# data.pop(1)
+
+
+# nilai_data = data.pop(1)
+# print(data)
+# print(nilai_data)
+
+
+
+# data_mahasiswa = [
+#     ["060", "ifnu", ["APD", "ORSIKOM"]],
+#     ["065", "adi"]
+#     ]
+
+# print(data_mahasiswa[0][1])
+# print(data_mahasiswa[0])
+# print(data_mahasiswa[0][2][0])
+
+
+# data_mahasiswa = [
+#     ["Admin", "admin123", "admin"]
+#     ["user", "user1234", "user"]
+#     ]
+
+# print(data_mahasiswa[0][2][0])
+
+#  buah = ["apel", "durian", "jeruk"]
+# index = 1
+# for index in range (len(buah)):
+#     print(f"data ke {index+1}")
+#     print (data)
+
+# index = 1
+# data_mahasiswa = [
+#     ["Admin", "admin123", "admin"]
+#     ["user", "user1234", "user"]
+#     ]
+
+
+buah = ["apel", "durian", "jeruk"]
+
+# buah1, buah2, buah3 = buah
+# print(buah1)
+# print(buah2)
+# print(buah3)
+
+
+import os 
+data_mahasiswa = ["ifnu", "adi","ucup","michael"]
+print(data_mahasiswa)
